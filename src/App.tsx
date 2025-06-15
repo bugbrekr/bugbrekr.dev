fa80fa33e8b2dd7be0ee912cc7e9acbfb9e8afe2
bugbrekr <me@bugbrekr.dev>
2025-06-15 22:44:05 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <h1>Welcome to BugBrekr's personal hosting space!</h1>
       <text>The landing page is still (might forever be) under construction, so check back later. <i>or not.</i></text>
-      <h3>Contact me at:</h3>
+      <h2>Contact me at:</h2>
       <p>Email: <a href="mailto:me@bugbrekr.dev">me@bugbrekr.dev</a></p>
     </>
   )
