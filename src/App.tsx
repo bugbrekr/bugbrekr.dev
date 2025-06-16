11f4ef1312bdb6fc8ae95729566329231a4605df
BugBrekr <chaitanyapy07@gmail.com>
2025-06-16 08:31:08 +0530

    Update text

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1>Welcome to BugBrekr's personal hosting space!</h1>
-      <text>The landing page is still (might forever be) under construction, so check back later. <i>or not.</i></text>
+      <text>This landing page is still (and might forever be) under construction, so check back later. <i>or not.</i></text>
       <h2>Contact me at:</h2>
       <p>Email: <a href="mailto:me@bugbrekr.dev">me@bugbrekr.dev</a></p>
     </>
